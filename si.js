71e84cf4dac7aeda24e3b64d9ec1577de1654d33
amanjeet365x27-max <amanjeet365x27@gmail.com>
2026-04-06 11:37:12 +0530

    Update si.js

--- si.js
+++ si.js
@@ -46,8 +46,6 @@ module.exports = {
         { name: "🧑 Humans", value: `\`${humans}\``, inline: true },
         { name: "🤖 Bots", value: `\`${bots}\``, inline: true },
         { name: "🟢 Online", value: `\`${online}\``, inline: true },
-        { name: "📥 Joins (24h)", value: `\`${last24hJoins}\``, inline: true },
-        { name: "📤 Leaves (24h)", value: `\`${last24hLeaves}\``, inline: true }
       )
       .setFooter({ text: "⚡ Powered by HEROIC HUSTLE" })
       .setTimestamp();
